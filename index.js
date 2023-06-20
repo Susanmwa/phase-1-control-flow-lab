c682a87322eb2bf2bf67662f2400421b849309a8
Mischel <susan.mwaura@student.moringaschool.com>
2023-06-20 14:01:25 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
-}
+  let response;
+  
+  if (distance <= 400) {
+    response = 'This one is on me!';
+  } else if (distance > 2000 && distance <= 2500) {
+    response = 'I will gladly take your thirty bucks.';
+  } else if (distance > 2500) {
+    response = 'No can do.';
+  }
+  
+  return response;
+ }
+  function ternaryCheckCity(city) {
+    let response;
+    if (city === 'NYC') {
+    response = "Ok, sounds good." ;}
+    else if (city !== 'NYC') {
+    response = "No go.";}
+    return response;
+    }
+    function switchOnCharmFromTip(tip){
+      let response;
+      if (tip === "generous"){
+        response = "Thank you so much." ;
+      }
+      else if (tip !== "generous"){
+        response = "Thank you.";
+      }
+      else  {
+        response = 'Bye.';
+      }
+    return response;
+    }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+  
+  
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+
